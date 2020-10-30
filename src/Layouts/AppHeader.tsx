9e9f2d90d8a8710082382a5516d8d13a7d71b45b
RyRy79261 <ryanjnoble@gmail.com>
2020-10-30 18:50:27 +0200

    Added logos

--- src/Layouts/AppHeader.tsx
+++ src/Layouts/AppHeader.tsx
@@ -30,6 +30,7 @@ const useStyles = makeStyles(({ constants, palette }: ITheme) => {
     logo: {
       height: constants.generalUnit * 5,
       width: constants.generalUnit * 5,
+      marginRight: constants.generalUnit * 3,
       "& svg, & img": {
         maxHeight: "100%",
         maxWidth: "100%",
@@ -65,9 +66,26 @@ const AppHeader: React.FC<IAppHeader> = () => {
     <header className={clsx(classes.root)}>
       <div className={classes.left}>
         {/* ADD LOGO HERE */}
-        {/* <div className={classes.logo}>
-        
-        </div> */}
+        <div className={classes.logo}>
+          <svg
+            viewBox="0 0 40 40"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M29.5455 12.4026L37.1429 20L20 37.1429L2.85714 20L20 2.85714L27.5974 10.4545L29.026 9.02597L20 0L0 20L20 40L40 20L30.974 10.974L29.5455 12.4026Z"
+              fill="#00A308"
+            />
+            <path
+              d="M15 23.5715L10 28.5715L11.4416 30L16.4416 25"
+              fill="#00A308"
+            />
+            <path
+              d="M10 11.4286L28.5714 30L30 28.5584L11.4416 10"
+              fill="#00A308"
+            />
+          </svg>
+        </div>
         <Typography variant="h4">ChainBridge Token Swap</Typography>
       </div>
       <section className={classes.state}>
