55548bed17cf2cc423a9f0aebef204627935a4f4
Michael Yankelev <myankelev@gmail.com>
2021-03-08 05:10:13 +0200

    add handlers for other events

--- src/Contexts/ExplorerContext.tsx
+++ src/Contexts/ExplorerContext.tsx
@@ -2,7 +2,7 @@ import React, { useContext, useEffect, useReducer } from "react";
 import { BridgeFactory } from "@chainsafe/chainbridge-contracts";
 import { providers } from "ethers";
 import { chainbridgeConfig } from "../chainbridgeConfig";
-import { transferReducer } from "./Reducers/TransferReducer";
+import { ProposalStatus, transferReducer } from "./Reducers/TransferReducer";
 
 interface IExplorerContextProps {
   children: React.ReactNode | React.ReactNode[];
@@ -30,28 +30,103 @@ const ExplorerProvider = ({ children }: IExplorerContextProps) => {
         );
         const depositFilter = bridgeContract.filters.Deposit(null, null, null);
         const depositLogs = await provider.getLogs(depositFilter);
-        console.log(depositLogs);
         depositLogs.forEach(async (dl) => {
           const parsedLog = bridgeContract.interface.parseLog(dl);
-          console.log(parsedLog);
           transfersDispatch({
             type: "addTransfer",
             payload: {
               depositNonce: parsedLog.args.depositNonce.toNumber(),
-              depositBlockNumber: dl.blockNumber,
-              depositTransactionHash: dl.transactionHash,
-              fromChainId: bridge.chainId,
-              fromNetworkName: bridge.name,
-              timestamp: (await provider.getBlock(dl.blockNumber)).timestamp,
-              toChainId: parsedLog.args.destinationChainID,
-              toNetworkName:
-                chainbridgeConfig.chains.find(
-                  (c) => c.chainId === parsedLog.args.destinationChainID
-                )?.name || "",
-              resourceId: parsedLog.args.resourceID,
+              transferDetails: {
+                depositBlockNumber: dl.blockNumber,
+                depositTransactionHash: dl.transactionHash,
+                fromChainId: bridge.chainId,
+                fromNetworkName: bridge.name,
+                timestamp: (await provider.getBlock(dl.blockNumber)).timestamp,
+                toChainId: parsedLog.args.destinationChainID,
+                toNetworkName:
+                  chainbridgeConfig.chains.find(
+                    (c) => c.chainId === parsedLog.args.destinationChainID
+                  )?.name || "",
+                resourceId: parsedLog.args.resourceID,
+              },
             },
           });
         });
+        console.log(`Added ${depositLogs.length} deposits`);
+
+        const proposalEventFilter = bridgeContract.filters.ProposalEvent(
+          null,
+          null,
+          null,
+          null,
+          null
+        );
+
+        const proposalEventLogs = await provider.getLogs(proposalEventFilter);
+        proposalEventLogs.forEach(async (pel) => {
+          const parsedLog = bridgeContract.interface.parseLog(pel);
+          transfersDispatch({
+            type: "addProposalEvent",
+            payload: {
+              depositNonce: parsedLog.args.depositNonce.toNumber(),
+              transferDetails: {
+                resourceId: parsedLog.args.resourceID,
+                fromChainId: parsedLog.args.originChainID,
+                fromNetworkName:
+                  chainbridgeConfig.chains.find(
+                    (c) => c.chainId === parsedLog.args.originChainID
+                  )?.name || "",
+                toChainId: bridge.chainId,
+                toNetworkName: bridge.name,
+              },
+              proposalEventDetails: {
+                proposalEventBlockNumber: pel.blockNumber,
+                proposalEventTransactionHash: pel.transactionHash,
+                dataHash: parsedLog.args.dataHash,
+                timestamp: (await provider.getBlock(pel.blockNumber)).timestamp,
+                proposalStatus: parsedLog.args.status,
+              },
+            },
+          });
+        });
+        console.log(`Added ${proposalEventLogs.length} proposal events`);
+
+        const proposalVoteFilter = bridgeContract.filters.ProposalVote(
+          null,
+          null,
+          null,
+          null
+        );
+
+        const proposalVoteLogs = await provider.getLogs(proposalVoteFilter);
+        proposalVoteLogs.forEach(async (pvl) => {
+          const parsedLog = bridgeContract.interface.parseLog(pvl);
+
+          transfersDispatch({
+            type: "addVote",
+            payload: {
+              depositNonce: parsedLog.args.depositNonce.toNumber(),
+              transferDetails: {
+                resourceId: parsedLog.args.resourceID,
+                fromChainId: parsedLog.args.originChainID,
+                fromNetworkName:
+                  chainbridgeConfig.chains.find(
+                    (c) => c.chainId === parsedLog.args.originChainID
+                  )?.name || "",
+                toChainId: bridge.chainId,
+                toNetworkName: bridge.name,
+              },
+              voteDetails: {
+                voteBlockNumber: pvl.blockNumber,
+                voteTransactionHash: pvl.transactionHash,
+                dataHash: parsedLog.args.dataHash,
+                timestamp: (await provider.getBlock(pvl.blockNumber)).timestamp,
+                voteStatus: parsedLog.args.status,
+              },
+            },
+          });
+        });
+        console.log(`Added ${proposalVoteLogs.length} proposal votes`);
       });
     };
 
