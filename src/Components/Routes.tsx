3078ea2f6a88eb864b6dc0aa7c19ed079a03c577
Karthikeyan <karthikeyan@cere.io>
2021-07-06 12:40:15 +0530

    remove wrap token functionality

--- src/Components/Routes.tsx
+++ src/Components/Routes.tsx
@@ -1,13 +1,11 @@
 import React from "react";
 import { Switch, Route, Redirect } from "@chainsafe/common-components";
 import TransferPage from "./Pages/TransferPage";
-import WrapperPage from "./Pages/WrapperPage";
 import ExplorerPage from "./Pages/ExplorerPage";
 import { ExplorerProvider } from "../Contexts/ExplorerContext";
 
 export const ROUTE_LINKS = {
   Transfer: "/transfer",
-  Wrap: "/wrap",
   Explore: "/explore",
 };
 
@@ -15,7 +13,6 @@ const FilesRoutes = () => {
   return (
     <Switch>
       <Route exact path={ROUTE_LINKS.Transfer} component={TransferPage} />
-      <Route exact path={ROUTE_LINKS.Wrap} component={WrapperPage} />
       <Route exact path={ROUTE_LINKS.Explore}>
         <ExplorerProvider>
           <ExplorerPage />
