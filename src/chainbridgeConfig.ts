d087559244c31fc932d21194f80d9c0ceb0cf33f
chris-shyft <chris@shyft.network>
2021-07-29 18:05:02 -0400

    [ fixed config issue ]

--- src/chainbridgeConfig.ts
+++ src/chainbridgeConfig.ts
@@ -1,5 +1,5 @@
 import ShyftIcon from "./media/tokens/shyft.svg";
-# import ETHIcon from "./media/tokens/eth.svg";
+// import ETHIcon from "./media/tokens/eth.svg";
 // import WETHIcon from "./media/tokens/weth.svg";
 
 export type TokenConfig = {
