b5bc45870872d066ee9082ba9f58152abf4951e7
Nicolas Riquelme <nrriquelme@gmail.com>
2021-09-08 09:02:20 -0300

    Fix duplicate messages that are displayed by the transferActive modal component

--- src/Contexts/Reducers/TransitMessageReducer.tsx
+++ src/Contexts/Reducers/TransitMessageReducer.tsx
@@ -9,7 +9,24 @@ export function transitMessageReducer(
 ) {
   switch (action.type) {
     case "addMessage":
-      return [...transitMessage, action.payload];
+      // NOTE: this is to avoid duplicate messages due to chain reorganization
+      const { payload } = action;
+      if (typeof payload === "object") {
+        const foundElement = transitMessage.find(
+          ({ address }: any) => address === payload.address
+        );
+
+        return foundElement !== undefined
+          ? transitMessage
+          : [...transitMessage, payload];
+      }
+
+      const foundMessage = transitMessage.find((item: any) => item === payload);
+
+      return foundMessage !== undefined
+        ? transitMessage
+        : [...transitMessage, payload];
+
     case "resetMessages":
       return [];
     default:
