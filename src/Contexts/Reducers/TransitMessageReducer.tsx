bf46656987eb61d2bf8331d2787523cf15b6c14c
Nikolay Topkaridi <enemycnt@gmail.com>
2021-09-29 14:04:30 +0300

    added filter messages for distinct values

--- src/Contexts/Reducers/TransitMessageReducer.tsx
+++ src/Contexts/Reducers/TransitMessageReducer.tsx
@@ -1,10 +1,10 @@
-import { Vote } from "../NetworkManagerContext";
+import { Vote, TransitMessage } from "../NetworkManagerContext";
 
-export type AddMessageAction = { type: "addMessage"; payload: string | Vote };
+export type AddMessageAction = { type: "addMessage"; payload: TransitMessage };
 export type ResetAction = { type: "resetMessages" };
 
 export function transitMessageReducer(
-  transitMessage: Array<string | Vote>,
+  transitMessage: Array<TransitMessage>,
   action: AddMessageAction | ResetAction
 ) {
   switch (action.type) {
@@ -12,22 +12,17 @@ export function transitMessageReducer(
       // NOTE: this is to avoid duplicate messages due to chain reorganization
       const { payload } = action;
 
-      if (typeof payload === "object") {
-        const foundElement = transitMessage.find(
-          ({ address }: any) => address === payload.address
-        );
-
-        return foundElement !== undefined
-          ? transitMessage
-          : [...transitMessage, payload];
-      }
-
-      const foundMessage = transitMessage.find((item: any) => item === payload);
-
-      return foundMessage !== undefined
-        ? transitMessage
-        : [...transitMessage, payload];
-
+      const messages = [...transitMessage, payload];
+      // Select distinct messages by address and eventType
+      const uniqueMessages = [
+        ...new Map(
+          messages.map((item) => [item.address + item.eventType, item])
+        ).values(),
+      ];
+      const uniqueMessagesSorted = uniqueMessages.sort(
+        (a, b) => a.order - b.order
+      );
+      return uniqueMessagesSorted;
     case "resetMessages":
       return [];
     default:
