e04127a38d0086be7ed5a3ad378b265578129ec1
Nikolay Topkaridi <enemycnt@gmail.com>
2021-10-18 17:49:03 +0300

    fix routing warnings

--- src/Components/Routes.tsx
+++ src/Components/Routes.tsx
@@ -23,12 +23,12 @@ const ChainbridgeRoutes: React.FC<IChainbridgeRoutes> = ({ wrapTokenPage }) => {
       {wrapTokenPage && (
         <Route exact path={ROUTE_LINKS.Wrap} component={WrapperPage} />
       )}
-      <Route exact path={ROUTE_LINKS.Explorer} component={ExplorerPage}>
+      <Route exact path={ROUTE_LINKS.Explorer}>
         <ExplorerProvider>
           <ExplorerPage />
         </ExplorerProvider>
       </Route>
-      <Route exact path={ROUTE_LINKS.ExplorerDetailed} component={ExplorerPage}>
+      <Route exact path={ROUTE_LINKS.ExplorerDetailed}>
         <ExplorerProvider>
           <ExplorerPage />
         </ExplorerProvider>
