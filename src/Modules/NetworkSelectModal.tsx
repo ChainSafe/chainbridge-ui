93b00b5dc45c5f3c04e1a8687e17438cca9b4b56
karthikeyan-cere <karthikeyan@cere.io>
2022-02-08 19:30:01 +0530

    style: reverted changes for network select modal

--- src/Modules/NetworkSelectModal.tsx
+++ src/Modules/NetworkSelectModal.tsx
@@ -8,22 +8,10 @@ import {
   ProgressBar,
   Typography,
 } from "@chainsafe/common-components";
-import styles from "../Constants/constants";
 
 const useStyles = makeStyles(({ constants, palette, zIndex }: ITheme) => {
   return createStyles({
-    root: {
-      borderRadius: 5,
-    },
-    title: {
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      fontSize: 28,
-      fontFamily: styles.primaryFont,
-      paddingTop: constants.generalUnit * 3,
-      paddingBottom: constants.generalUnit * 4,
-    },
+    root: {},
     slide: {
       borderRadius: constants.generalUnit / 2,
       padding: `${constants.generalUnit}px ${constants.generalUnit * 2}px`,
@@ -38,26 +26,6 @@ const useStyles = makeStyles(({ constants, palette, zIndex }: ITheme) => {
       display: "flex",
       flexDirection: "row",
       justifyContent: "space-around",
-    },
-    ethButton: {
-      fontFamily: styles.secondaryFont,
-      background: "linear-gradient(105.79deg, #6300E1 1.84%, #DD00E1 92.41%)",
-      fontWeight: "bold",
-      borderRadius: 5,
-      letterSpacing: 1,
-      "&:hover": {
-        color: "white",
-      },
-    },
-    substrateButton: {
-      fontFamily: styles.secondaryFont,
-      background: "linear-gradient(105.79deg, #9A1AFF 38.7%, #0028FB 102.94%)",
-      fontWeight: "bold",
-      borderRadius: 5,
-      letterSpacing: 1,
-      "&:hover": {
-        color: "white",
-      },
     },
   });
 });
@@ -78,30 +46,20 @@ const NetworkSelectModal = () => {
     >
       {walletType === "select" && (
         <>
-          <div className={classes.title}>Please select a wallet type</div>
+          <Typography variant="h3" component="p">
+            Please select a wallet type
+          </Typography>
           <section className={classes.buttons}>
             {chains?.every((item) => item.type === "Ethereum") ? (
-              <Button
-                variant="primary"
-                className={classes.ethButton}
-                onClick={() => setWalletType("Ethereum")}
-              >
+              <Button onClick={() => setWalletType("Ethereum")}>
                 Use Ethereum wallet
               </Button>
             ) : (
               <>
-                <Button
-                  variant="primary"
-                  className={classes.ethButton}
-                  onClick={() => setWalletType("Ethereum")}
-                >
+                <Button onClick={() => setWalletType("Ethereum")}>
                   Use Ethereum wallet
                 </Button>
-                <Button
-                  variant="primary"
-                  className={classes.substrateButton}
-                  onClick={() => setWalletType("Substrate")}
-                >
+                <Button onClick={() => setWalletType("Substrate")}>
                   Use Substrate wallet
                 </Button>
               </>
@@ -111,7 +69,9 @@ const NetworkSelectModal = () => {
       )}
       {walletType === "Substrate" && (
         <>
-          <div className={classes.title}>Connecting to node</div>
+          <Typography variant="h2" component="p">
+            Connecting to node
+          </Typography>
           <ProgressBar size="small" variant="primary" />
         </>
       )}
