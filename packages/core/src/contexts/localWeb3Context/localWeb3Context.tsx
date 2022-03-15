5184ec99ab8012d21fb30569f6637d09453cf740
Nikolay Topkaridi <enemycnt@gmail.com>
2022-03-15 15:35:18 +0300

    remove unusued code from local web3 context

--- packages/core/src/contexts/localWeb3Context/localWeb3Context.tsx
+++ packages/core/src/contexts/localWeb3Context/localWeb3Context.tsx
@@ -21,7 +21,7 @@ const LocalProviderContext = React.createContext<LocalWeb3Context | undefined>(
   undefined
 );
 
-const createLocalProvider = ({
+const LocalProvider = ({
   children,
   externalProvider,
   useExternalProvider,
@@ -175,103 +175,6 @@ const createLocalProvider = ({
     </LocalProviderContext.Provider>
   );
 }
-
-const createExternalProvider = ({
-  children,
-  externalProvider,
-  tokensToWatch,
-  onboardConfig,
-  cacheWalletSelection = true,
-  networkIds,
-  checkNetwork = (networkIds && networkIds.length > 0) || false,
-  spenderAddress,
-}: LocalWeb3ContextProps) => {
-  const [state, dispatcher] = useReducer(localWeb3ContextReducer, {
-    savedWallet: ""
-  } as any);
-
-  const [eNetwork, setENetwork] = useState<ethers.providers.Network>()
-  const [externalAddress, setExternalAddress] = useState<string>()
-  const [balance, setBalance] = useState<number>()
-  useEffect(() => {
-
-    async function getNetworkInfo() {
-      const signer = externalProvider.getSigner();
-      const accountAddress = await signer.getAddress()
-      const balance = await externalProvider.getBalance(accountAddress)
-      setBalance( Number(ethers.utils.formatEther(balance)))
-      setExternalAddress(accountAddress)
-      const externalNetworkInfo = await externalProvider.getNetwork()
-      setENetwork(externalNetworkInfo)
-    }
-    getNetworkInfo()
-  }, [])
-
-  useEffect(() => {
-    const networkTokens =
-      (tokensToWatch && eNetwork && tokensToWatch[eNetwork.chainId]) || [];
-
-    let tokenContracts: Array<Erc20Detailed> = [];
-    if (externalProvider && externalAddress && networkTokens.length > 0) {
-      getTokenDataDirect(networkTokens, dispatcher, externalAddress, externalProvider, spenderAddress);
-    }
-    return () => {
-      if (tokenContracts.length > 0) {
-        tokenContracts.forEach((tc) => {
-          tc.removeAllListeners();
-        });
-        tokenContracts = [];
-        dispatcher({ type: "resetTokens" });
-      }
-    };
-  }, [eNetwork, externalAddress]);
-
-  const {
-    address,
-    provider,
-    network,
-    wallet,
-    onboard,
-    ethBalance,
-    tokens,
-    isReady,
-    gasPrice,
-    walletConnectReady,
-    savedWallet
-  }: LocalWeb3State = state;
-
-  return (
-    <LocalProviderContext.Provider
-      value={{
-        address: externalAddress,
-        ethBalance: balance,
-        isReady: true,
-        network: eNetwork?.chainId,
-        onboard: onboard,
-        provider: externalProvider,
-        savedWallet: "",
-        tokens: tokens,
-        wallet: undefined,
-
-        gasPrice,
-        isMobile: false,
-        walletConnectReady: false,
-
-        useExternalProvider,
-
-        resetOnboard,
-        refreshGasPrice,
-        signMessage,
-        checkIsReady,
-        dispatcher,
-      }}
-    >
-      {eNetwork ? children : null}
-    </LocalProviderContext.Provider>
-  );
-}
-
-const LocalProvider = (props: LocalWeb3ContextProps) => createLocalProvider(props)
 
 const useWeb3 = () => {
   const context = React.useContext(LocalProviderContext);
