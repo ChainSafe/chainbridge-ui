00ef1419071c17b35869b8d5176f0a759cff1d5d
Nikolay Topkaridi <enemycnt@gmail.com>
2022-08-01 17:48:20 +0300

    added docker secrets for config server

--- packages/config-server/src/index.ts
+++ packages/config-server/src/index.ts
@@ -1,11 +1,12 @@
 import express, { Application } from "express";
 import cors from "cors";
+import { secrets } from "docker-secret";
 
 import { SSM } from "@aws-sdk/client-ssm";
 
 const PORT = process.env.PORT || 8000;
 const HOST = process.env.HOST || 'localhost';
-const SSM_PARAMETER_NAME = process.env.SSM_PARAMETER_NAME
+const SSM_PARAMETER_NAME = secrets.SSM_PARAMETER_NAME || process.env.SSM_PARAMETER_NAME
 
 const app: Application = express();
 app.use(cors());
@@ -15,12 +16,16 @@ const ssm = new SSM({
 });
 
 const getConfigFromSSM = async () => {
+  console.log("HOST", HOST)
+  console.log("PORT", PORT)
+  console.log("SSM_PARAM", SSM_PARAMETER_NAME)
   try {
     const data = await ssm.getParameter({
       Name: SSM_PARAMETER_NAME,
       WithDecryption: true,
     });
     const rawResponse = data.Parameter?.Value;
+    console.log("🚀 ~ file: index.ts ~ line 24 ~ getConfigFromSSM ~ rawResponse", rawResponse)
     if (rawResponse) {
       const parsedResponse = JSON.parse(rawResponse);
       return parsedResponse;
